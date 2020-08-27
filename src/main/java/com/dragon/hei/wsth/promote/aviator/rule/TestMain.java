57455c61046304f5bc64d3275ef48aa031877d7e
lilong <lilong@ihomefnt.com>
2020-08-27 15:35:56 +0800

    main函数路径变更

---
+++ src/main/java/com/dragon/hei/wsth/promote/aviator/rule/TestMain.java
@@ -0,0 +1,67 @@
+package com.dragon.hei.wsth.promote.aviator.rule;
+
+import com.alibaba.fastjson.JSON;
+import com.dragon.hei.wsth.vo.aviator.AviatorContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: lilong
+ **/
+class TestMain{
+
+    public static void main(String[] args) {
+
+
+        // aviator上下文
+        AviatorContext context = buildContext();
+
+        // 存储表达式执行结果
+        List<AbstractAviatorRule> response = new ArrayList<>();
+
+        // 规则chain
+        RuleChain ruleChain = new DefaultRuleChain(rules());System.out.println("等待。。。");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ruleChain.matchRule(context, response);System.out.println("等待。。。");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(JSON.toJSONString(response));
+    }
+
+    private static AviatorContext buildContext(){
+        AviatorContext context = new AviatorContext();
+        context.setName("宋小宝");
+        context.setAge(35);
+        context.setClazz(3);
+        context.setBonus(10d);
+        context.setSalNum(20);
+        return context;
+    }
+
+    private static List<AbstractAviatorRule> rules(){
+
+        AbstractAviatorRule rule1 = new DefaultAviatorRule();
+        rule1.setRuleId(1001l);
+        rule1.setExpression("age <= 40 && clazz >= 3 && string.startsWith(name, '宋')");
+
+        AbstractAviatorRule rule2 = new CalculatorAviatorRule();
+        rule2.setRuleId(1002l);
+        rule2.setExpression("salNum * bonus * 10/100");
+
+        List<AbstractAviatorRule> rules = new ArrayList<>();
+        rules.add(rule1);
+        rules.add(rule2);
+        return rules;
+    }
+}
